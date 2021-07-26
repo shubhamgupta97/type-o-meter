bfef6dd4c0ce1f805c31b547ddda7e65752d7d07
Shubham Gupta <gupta.shubham97@gmail.com>
2021-07-27 00:01:38 +0530

    Add quotes to data.js

--- data.js
+++ data.js
@@ -14,5 +14,29 @@ const data = [
     {
         "text": "I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.",
         "source": "Frank Herbert, Dune"
+    },
+    {
+        "text": "All the world’s a stage, And all the men and women merely players: They have their exits and their entrances; And one man in his time plays many parts.",
+        "source": "William Shakespeare, As You Like It"
+    },
+    {
+        "text": "To be, or not to be: that is the question: Whether ’tis nobler in the mind to suffer; The slings and arrows of outrageous fortune, Or to take arms against a sea of troubles, And by opposing end them. To die - to sleep...",
+        "source": "William Shakespeare, Hamlet"
+    },
+    {
+        "text": "I am an invisible man. No, I am not a spook like those who haunted Edgar Allan Poe; nor am I one of your Hollywood-movie ectoplasms. I am a man of substance, of flesh and bone, fiber and liquids - and I might even be said to possess a mind. I am invisible, understand, simply because people refuse to see me.",
+        "source": "Ralph Ellison, Invisible Man"
+    },
+    {
+        "text": "Because it is my name! Because I cannot have another in my life! Because I lie and sign myself to lies! Because I am not worth the dust on the feet of them that hang! How may I live without my name? I have given you my soul; leave me my name!",
+        "source": "Arthur Miller, The Crucible"
+    },
+    {
+        "text": "He who has felt the deepest grief is best able to experience supreme happiness. Live, then, and be happy, beloved children of my heart, and never forget, that until the day God will deign to reveal the future to man, all human wisdom is contained in these two words: 'Wait and Hope.'",
+        "source": "Alexandre Dumas, The Count of Monte Cristo"
+    },
+    {
+        "text": "It was a dark and stormy night; the rain fell in torrents, except at occasional intervals, when it was checked by a violent gust of wind which swept up the streets (for it is in London that our scene lies), rattling along the house-tops, and fiercely agitating the scanty flame of the lamps that struggled against the darkness.",
+        "source": "Edward George Bulwer-Lytton, Paul Clifford"
     }
 ];
